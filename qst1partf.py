2f137ae6841aecad4abc28e8a176388a1713339a
Mahalia E <maev8907@colorado.edu>
2017-03-23 16:35:35 -0600

    Update qst1partf.py

--- qst1partf.py
+++ qst1partf.py
@@ -2,7 +2,7 @@
 # Shakespeare’s play Much Ado About Nothing, Act 1 Scene 2
 
 # Step 1: Write a function that takes as input the data MuchAdo freqs file for PS6 
-file = open("data_MuchAdo_txt.text", "r")
+file = open("csci3104_S2017_PS6_data_muchAdo_freqs.txt", "r")
 print file.read()
 
 # Step 2: Outputs a file x that contains n characters drawn randomly but with the given frequencies
