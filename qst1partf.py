cbec6c5985da9fd070674fc4511b4f8adfc9b3f7
Mahalia E <maev8907@colorado.edu>
2017-03-23 16:33:44 -0600

    Create qst1partf.py

---
+++ qst1partf.py
@@ -0,0 +1,10 @@
+# Question 1 - Part f 
+# Shakespeare’s play Much Ado About Nothing, Act 1 Scene 2
+
+# Step 1: Write a function that takes as input the data MuchAdo freqs file for PS6 
+file = open("data_MuchAdo_txt.text", "r")
+print file.read()
+
+# Step 2: Outputs a file x that contains n characters drawn randomly but with the given frequencies
+
+# Step 3: Using your string alignment functions, determine what value of n is required to produce an overlap of 7 characters
